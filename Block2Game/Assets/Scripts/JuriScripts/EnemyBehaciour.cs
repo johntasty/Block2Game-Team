5b47073f4017b076901b5b54557241db767b1deb
HansDagger <juri@gritz.de>
2022-12-17 12:08:27 +0100

    attempt at lerp stuff
    tried implementing enemy movement following the players points using nester lerps

--- Block2Game/Assets/Scripts/JuriScripts/EnemyBehaciour.cs
+++ Block2Game/Assets/Scripts/JuriScripts/EnemyBehaciour.cs
@@ -9,6 +9,13 @@ public class EnemyBehaciour : MonoBehaviour
     private Rigidbody rb;
     private Vector3 movement;
     public float dashPower = 25f;
+    private float lerpTime = 0;
+    private Vector3 startPoint;
+    private Vector3 endPoint;
+    private Vector3 midPoint;
+    public PlayerScript pScript;
+    [SerializeField] float lerpSpeed = 1f;
+    public Vector3 lerpValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,7 +39,21 @@ public class EnemyBehaciour : MonoBehaviour
 
     void moveCharacter(Vector3 direction)
     {
-        rb.MovePosition((Vector3)transform.position + (direction * moveSpeed * Time.deltaTime));
+        //rb.MovePosition((Vector3)transform.position + (direction * moveSpeed * Time.deltaTime));
+        if (pScript._positions.Count > 3)
+        {
+            while (lerpTime < 1)
+            {
+                startPoint = pScript._positions[0]; //grab all the points to move with
+                endPoint = pScript._positions[2];
+                midPoint = pScript._positions[1];
+                transform.position = Vector3.Lerp(              //nested lerps to make curve
+                Vector3.Lerp(startPoint, midPoint, lerpTime),
+                Vector3.Lerp(midPoint, endPoint, lerpTime),
+                lerpTime);
+                lerpTime += Time.deltaTime * lerpSpeed;
+            }
+        }
     }
 
     //trigger enemy to drive at opponent quickly when the two are in close proximity
