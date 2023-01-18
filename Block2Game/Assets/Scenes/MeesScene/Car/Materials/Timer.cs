f55eca9f0e71867dc47dcf405787dd4ebb603498
Moose-the-Goose <114147610+Moose-the-Goose@users.noreply.github.com>
2023-01-18 12:18:53 +0100

    Countdown animation done

--- Block2Game/Assets/Scenes/MeesScene/Car/Materials/Timer.cs
+++ Block2Game/Assets/Scenes/MeesScene/Car/Materials/Timer.cs
@@ -5,46 +5,55 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    
-    public AnimationClip Countdown3;
-    public AnimationClip Countdown2;
-    public AnimationClip Countdown1;
-    private int CurrentCountdown;
-    private TMP_Text Text;
-    Animation anim;
-    // Start is called before the first frame update
-    private void Start()
-    {
-        anim = GetComponent<Animation>();
-        GetComponent<Animator>().enabled = false;
-        Text = GetComponent<TMP_Text>();
-        CurrentCountdown = 3;
-        StartCoroutine(startcountdown());
 
+    Animator m_animator;
+    TMP_Text m_text;
+    Canvas canvas;
+    private int m_current_number;
+    private int m_initial_waittime;
+
+    void Start()
+    {
+        // Set the initial countdown number to 3
+        m_current_number = 3;
+
+        // Before starting the countdown, wait this many seconds. 
+        m_initial_waittime = 2;
+
+        // Get all the required components once during start. 
+        m_animator = GetComponent<Animator>();
+        m_text = GetComponent<TMP_Text>();
+        canvas = GetComponentInParent<Canvas>();
+
+        // Disable the animator to not autostart the animation. 
+        m_animator.enabled = false;
+
+        // Start a coroutine.
+        StartCoroutine(delayAnimation());
     }
 
 
-    public IEnumerator startcountdown()
+    // Does the actual hard work; waits m_initial_waittime seconds, 
+    // and then counts down from 3 to 0. 
+    IEnumerator delayAnimation()
     {
-        yield return new WaitForSeconds(5);
-        while (CurrentCountdown > 0)
+        yield return new WaitForSeconds(m_initial_waittime);
+        // Can do other things to, such as changing color
+        m_text.color = Color.red;
+        m_animator.enabled = true;
+        while (m_current_number > 0)
         {
-            GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
-
-            GetComponent<Animator>().enabled = true;
-
             yield return new WaitForSeconds(1);
-            CurrentCountdown -= 1;
-            Text.text = CurrentCountdown.ToString();
-            Debug.Log(Text.text);
-            GetComponent<Animator>().enabled = false;
-            Debug.Log(GetComponent<RectTransform>().localScale);
-
-            GetComponent<RectTransform>().localScale = new Vector3(1,1,1);
-            Debug.Log(GetComponent<RectTransform>().localScale);
+            m_current_number -= 1/2;
+            Debug.Log(m_current_number -= 1);
+            m_text.text = m_current_number.ToString();
         }
-        //Text.text = (string) (int) CurrentCountdown;
-        //ani.Play("countdown text", 0);
 
+        // Stop playing the animations. 
+        m_animator.enabled = false;
+
+        // Optionally destroy the gameobject when no longer needed.
+        canvas.enabled = false;
     }
 }
+
